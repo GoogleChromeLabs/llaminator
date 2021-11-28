ac3eefe80f4a0d241fdf358c07d1957bf348d7d9
Glenn Hartmann <hartmanng@chromium.org>
2021-11-28 17:20:56 -0500

    Add `npm run-script export-dev`
    Compiles the app, then exports the static
    result into a "dist_dev" directory.
    
    This is a "dev" rather than production build,
    since it's not minified and contains source maps.
    
    Note that this doesn't actually work at the moment,
    because index.ts's "MIME type ('video/mp2t') is not executable."
    In other words, we'll need to change the file extension to
    .js?

---
+++ rollup.config.export-dev.mjs
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import clear from 'rollup-plugin-clear';
+import copy from 'rollup-plugin-copy';
+import esbuild from 'rollup-plugin-esbuild';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import { replaceConfig } from './build_common.mjs';
+
+const outDir = 'dist_dev';
+const entryFileNamesPattern = '[name].ts';
+const replacePluginInstance = replace(replaceConfig('/'));
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: outDir,
+      sourcemap: true,
+      entryFileNames: entryFileNamesPattern,
+    },
+    plugins: [
+      clear({ targets: [outDir] }),
+      replacePluginInstance,
+      esbuild({}),
+      resolve(),
+      copy({
+        targets: [
+          {
+            src: ['src/**/*.*', '!src/**/*.ts'],
+            dest: outDir,
+          },
+        ],
+      }),
+    ],
+  },
+  {
+    input: 'src/sw.ts',
+    output: {
+      dir: outDir,
+      sourcemap: true,
+      entryFileNames: entryFileNamesPattern,
+    },
+    plugins: [
+      replacePluginInstance,
+      esbuild({}),
+      resolve(),
+    ],
+  },
+]
